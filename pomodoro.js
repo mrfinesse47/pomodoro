b6b1d55735adb837c0e6e29c7d0a0456f21c6e74
Kevin Mason <krmason2020@gmail.com>
2022-04-08 21:48:34 -0700

    figured out a way to have 3 sizes of pomodoro by possibly hiding un hiding

--- pomodoro.js
+++ pomodoro.js
@@ -4,5 +4,15 @@
 
 const PI = 3.14159265358979;
 
-const pomodoro = document.getElementById("pomodoro");
+const pomodoro = document.getElementById("pomodoro-time");
 pomodoro.style.strokeDasharray = 0.99 * 2 * PI * 139;
+
+const circleBG = document.getElementsByClassName("circle-bg");
+console.log(circleBG[0].outerHTML);
+
+const circle = document.getElementsByClassName("circle");
+console.log(circle[0].outerHTML);
+
+{
+  /* <circle id="pomodoro-time" class="circle" cx="198.3" cy="217.3" r="139.2" transform="rotate(270 198.3 217.3)" style="stroke-dasharray: 864.629;"></circle> */
+}
