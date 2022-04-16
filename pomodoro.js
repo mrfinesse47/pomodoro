971424c8cc01608c5f695992a63a837705574c68
Kevin Mason <krmason2020@gmail.com>
2022-04-15 18:58:21 -0700

    fixed bug where dash around circle repeated so i made the invisible dash super long

--- pomodoro.js
+++ pomodoro.js
@@ -4,10 +4,29 @@
 
 const PI = 3.14159265358979;
 const RADIUS = 159.5;
-let timeGonePercent = 85;
+// let timeGonePercent = 15;
 
-const pomodoro = document.getElementById("pomodoro-time");
-pomodoro.style.strokeDasharray = timeGonePercent * 0.01 * 2 * PI * RADIUS;
+// const pomodoro = document.getElementById("pomodoro-time");
+//pomodoro.style.strokeDasharray = timeGonePercent * 0.01 * 2 * PI * RADIUS;
+//300%2c 3700'
+// pomodoro.style.strokeDasharray = `${
+//   timeGonePercent * 0.01 * 2 * PI * RADIUS
+// }, 36000`;
 
-const timeRemaining = document.getElementById("time-remaining");
-timeRemaining.innerText = "87";
+// const timeRemaining = document.getElementById("time-remaining");
+// timeRemaining.innerText = "20";
+
+setCountPercent(9);
+
+function setCountPercent(percentage) {
+  const PI = 3.14159265358979;
+  const RADIUS = 159.5;
+  const pomodoro = document.getElementById("pomodoro-time");
+  const timeRemaining = document.getElementById("time-remaining");
+  //pomodoro.style.strokeDasharray = percentage * 0.01 * 2 * PI * RADIUS;
+  pomodoro.style.strokeDasharray = `${
+    percentage * 0.01 * 2 * PI * RADIUS
+  }, 36000`;
+
+  timeRemaining.innerText = percentage;
+}
