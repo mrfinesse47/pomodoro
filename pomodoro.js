8b603c6c05b386e41ebfe3e85a6a5d1172bb92c2
Kevin Mason <krmason2020@gmail.com>
2022-04-15 19:19:02 -0700

    refactor script a little to clean up code

--- pomodoro.js
+++ pomodoro.js
@@ -2,28 +2,13 @@
 //   // Handler when the DOM is fully loaded
 // });
 
-const PI = 3.14159265358979;
-const RADIUS = 159.5;
-// let timeGonePercent = 15;
-
-// const pomodoro = document.getElementById("pomodoro-time");
-//pomodoro.style.strokeDasharray = timeGonePercent * 0.01 * 2 * PI * RADIUS;
-//300%2c 3700'
-// pomodoro.style.strokeDasharray = `${
-//   timeGonePercent * 0.01 * 2 * PI * RADIUS
-// }, 36000`;
-
-// const timeRemaining = document.getElementById("time-remaining");
-// timeRemaining.innerText = "20";
-
-setCountPercent(9);
+setCountPercent(75);
 
 function setCountPercent(percentage) {
   const PI = 3.14159265358979;
   const RADIUS = 159.5;
   const pomodoro = document.getElementById("pomodoro-time");
   const timeRemaining = document.getElementById("time-remaining");
-  //pomodoro.style.strokeDasharray = percentage * 0.01 * 2 * PI * RADIUS;
   pomodoro.style.strokeDasharray = `${
     percentage * 0.01 * 2 * PI * RADIUS
   }, 36000`;
