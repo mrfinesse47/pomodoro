a36d142f65a5e7730b92d36cfe8d9ecaa30cb937
Kevin Mason <krmason2020@gmail.com>
2022-04-17 14:27:23 -0700

    made an interval to update every second

--- pomodoro.js
+++ pomodoro.js
@@ -2,16 +2,27 @@
 //   // Handler when the DOM is fully loaded
 // });
 
-setCountPercent(95);
+let totalTime = 1 * 60; //25 minutes
+let timeRemaining = totalTime;
+let percentRemaining = (timeRemaining / totalTime) * 100;
+
+setCountPercent(percentRemaining);
+
+window.setInterval(() => {
+  timeRemaining -= 1;
+  percentRemaining = (timeRemaining / totalTime) * 100;
+  setCountPercent(percentRemaining);
+}, 1000);
 
 function setCountPercent(percentage) {
   const PI = 3.14159265358979;
   const RADIUS = 159.5;
-  const pomodoro = document.getElementById("pomodoro-time");
-  const timeRemaining = document.getElementById("time-remaining");
-  pomodoro.style.strokeDasharray = `${
-    percentage * 0.01 * 2 * PI * RADIUS
+  const pomodoroEL = document.getElementById("pomodoro-time");
+
+  const timeRemainingEL = document.getElementById("time-remaining");
+  pomodoroEL.style.strokeDasharray = `${
+    percentage * 0.01 * 2 * PI * RADIUS //(100-percentage) to count up
   }, 36000`;
 
-  timeRemaining.innerText = percentage;
+  timeRemainingEL.innerText = timeRemaining;
 }
